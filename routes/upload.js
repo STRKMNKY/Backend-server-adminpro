bb5af403de9c66d6c5ca70efada4c8566411c1e4
Emmanuel Ramirez <jsEmmanuelRamire@localhost.localdomain>
2019-02-27 15:56:59 -0600

    Arreglado Bug de creacion de imagenes vacias

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,9 @@
 // imports necesarios
 var express = require('express');
 var app = express();
+var Usuario = require('../models/usuario');
+var Medico = require('../models/medico');
+var Hospital = require('../models/hospital');
 
 //fielupload import
 var fileUpload = require('express-fileupload');
@@ -72,11 +75,13 @@ app.put('/:tipo/:id', (request, response) => {
     //Mover archivo del temeporal a un path
     var path = `./uploads/${ tipo }/${ nombre }`;
 
+    //Metodo bueno para mover archivos
+
     try {
 
-        //Metodo bueno para mover archivos
         fs.createReadStream(archivo.tempFilePath)
-        fs.createWriteStream(path);
+        var wr = fs.createWriteStream(path);
+        wr.write(archivo.data);
 
         return response.status(200).json({
             ok: true,
@@ -92,22 +97,6 @@ app.put('/:tipo/:id', (request, response) => {
         });
 
     }
-
-    /* fs.rename(archivo.tempFilePath, path, error => {
-         if (error) {
-             return response.status(500).json({
-                 ok: false,
-                 mensaje: 'error al mover archivo',
-                 errors: error
-             });
-         }
-
-         response.status(200).json({
-             ok: true,
-             mensaje: 'Archivo movido'
-         });
-
-     });*/
     /*
         archivo.mv(path, (error) => {
             if (error) {
@@ -128,5 +117,106 @@ app.put('/:tipo/:id', (request, response) => {
     */
 });
 
-//exportar
+function subirTipo(tipo, id, nombreArchivo) {
+
+    if (tipo === 'usuarios') {
+
+        Usuario.findById(id, (error, usuario) => {
+
+            if (error) {
+                return response.status('500').json({
+                    ok: false,
+                    message: 'Usuario no encontrado para el ID: ' + id
+                });
+            }
+
+            var oldPath = './uploads/usuarios/' + usuario.img;
+
+            //Valida la existencia de una imagen previa, en caso de que exista borra lo que esta en el oldpath
+
+            if (fs.existsSync(oldPath)) {
+                fs.unlink(oldPath);
+            }
+
+            usuario.img = nombreArchivo;
+
+            usuario.save((error, usuarioGuardado) => {
+
+                return response.status(200).json({
+                    ok: true,
+                    message: 'Imagen actualizada',
+                    usuario: 'ID: ' + usuarioGuardado.id + 'Nombre' + usuarioGuardado.nombre,
+                    imagen: usuario.img
+                });
+
+            });
+
+        });
+
+    }
+
+    if (tipo === 'medicos') {
+        Medico.findById(id, (error, medico) => {
+
+            if (error) {
+                return response.status('500').json({
+                    ok: false,
+                    message: 'Medico no encontrado para el ID: ' + id
+                });
+            }
+
+            var oldPath = './uploads/medicos/' + medico.img;
+
+
+            if (fs.existsSync(oldPath)) {
+                fs.unlink(oldPath);
+            }
+
+            medico.img = nombreArchivo;
+
+            medico.save((error, medicoGuardado) => {
+                return response.status(200).json({
+                    ok: true,
+                    message: 'Imagen actualizada',
+                    medico: 'ID: ' + medicoGuardado.id + 'Nombre: ' + medicoGuardado.nombre,
+                    imagen: medicoGuardado.img
+                });
+            });
+
+        });
+    }
+
+    if (tipo === 'hospitales') {
+        Hospital.findById(id, (error, hospital) => {
+
+            if (error) {
+                return response.status('500').json({
+                    ok: false,
+                    message: 'Hospital no encontrado para el ID: ' + id
+                });
+            }
+
+            var oldPath = './uploads/hospitales' + hospital.img;
+
+            if (fs.existsSync(oldPath)) {
+                fs.unlink(oldPath);
+            }
+
+            hospital.img = nombreArchivo;
+
+            hospital.save((error, hospitalGuardado) => {
+                return response.status(200).json({
+                    ok: true,
+                    message: 'Image actualizada',
+                    hospital: 'ID: ' + hospitalGuardado.id + 'Nombre: ' + hospitalGuardado.nombre,
+                    imagen: hospitalGuardado.img
+                });
+            });
+
+        });
+    }
+
+}
+
+//exportarimag
 module.exports = app;
